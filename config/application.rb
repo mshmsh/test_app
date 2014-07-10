48b134d6ba48dc20a7cb90786b0e7f993fca6e8f
mshmsh <wannabemillioner@gmail.com>
2014-07-10 04:31:41 +0430

    uncommnet application.yaml

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config =  YAML.load(File.read(File.expand_path('../application.yml',__FILE__)))
-config.merge! config.fetch(Rails.env, {})
-config.each do |key, value|
-    ENV[key] = value.to_s unless value.kind_of? Hash
-end
+# config =  YAML.load(File.read(File.expand_path('../application.yml',__FILE__)))
+# config.merge! config.fetch(Rails.env, {})
+# config.each do |key, value|
+#     ENV[key] = value.to_s unless value.kind_of? Hash
+# end
 
 module Spirit
   class Application < Rails::Application
